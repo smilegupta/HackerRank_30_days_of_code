9e593a894aa2e27220bbff75cce823280dd5d797
Smile Gupta <guptamiley3012@gmail.com>
2020-02-21 00:08:53 +0530

    Create Day16.cpp

---
+++ Day16.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    string S;
+    cin >> S;
+    try {
+   cout<<stoi(S);
+   } catch (...) {
+   cout<<"Bad String";
+   } 
+    return 0;
+}
