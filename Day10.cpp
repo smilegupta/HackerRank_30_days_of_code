1188daa3995d07200dc9de4727ba03aeb37bb4af
Smile Gupta <guptamiley3012@gmail.com>
2020-02-20 18:44:45 +0530

    Create Day10.cpp

---
+++ Day10.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+  
+int maxConsecutiveOnes(int x) 
+{ 
+    
+    int count = 0; 
+  
+    
+    while (x!=0) 
+    { 
+        x = (x & (x << 1)); 
+  
+        count++; 
+    } 
+  
+    return count; 
+} 
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout<<maxConsecutiveOnes(n);
+
+    return 0;
+}
