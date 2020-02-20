d3d48fc518210d4935fece12dd1163791c088e9b
Smile Gupta <guptamiley3012@gmail.com>
2020-02-20 22:26:40 +0530

    Create Day13.cpp

---
+++ Day13.cpp
@@ -0,0 +1,52 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Book{
+    protected:
+        string title;
+        string author;
+    public:
+        Book(string t,string a){
+            title=t;
+            author=a;
+        }
+        virtual void display()=0;
+
+};
+
+
+    class MyBook : public Book{
+   
+   int price=0;
+    public:
+
+MyBook(string title,string author,int p) : Book(title,author){
+
+price = p;
+
+}
+void display(){
+
+    cout<<"Title: "<<title<<endl;
+
+    cout<<"Author: "<<author<<endl;
+
+    cout<<"Price: "<<price;
+    
+}
+};
+
+int main() {
+    string title,author;
+    int price;
+    getline(cin,title);
+    getline(cin,author);
+    cin>>price;
+    MyBook novel(title,author,price);
+    novel.display();
+    return 0;
+}
